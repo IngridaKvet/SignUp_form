626e9fe283761da2d8e53c217eb2d0e076e751a8
Ingrida <ingridakvetkovskyte@gmail.com>
2023-05-11 15:37:59 +0300

    Fix psw not matching validation

--- script.js
+++ script.js
@@ -16,13 +16,20 @@ inputAll.forEach((item) => {
   document.getElementById("Pass1").addEventListener("change", validatePassword);
   document.getElementById("Pass2").addEventListener("change", validatePassword);
 
-  function validatePassword(){
+	function validatePassword(){
 		let pass1 = document.getElementById("Pass1").value;
     let pass2 = document.getElementById("Pass2").value;
 
     if((pass1 != pass2)&&(pass2 != "")) {
       document.getElementById("errorMsg").innerHTML="Passwords Don\'t Match";
+			document.getElementById('Pass1').setCustomValidity('Passwords Don\'t Match');
+			document.getElementById('Pass2').setCustomValidity('Passwords Don\'t Match');
+			
     	} else {
         document.getElementById('errorMsg').innerHTML='';
+				document.getElementById('Pass1').setCustomValidity('');
+				document.getElementById('Pass2').setCustomValidity('');
+
     }   
+
   }
