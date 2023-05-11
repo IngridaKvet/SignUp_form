5f52f128a1b79dfab64dc26428ecc7aa5bfd9ed5
Ingrida <ingridakvetkovskyte@gmail.com>
2023-05-11 15:02:40 +0300

    Add password matching validation

--- script.js
+++ script.js
@@ -5,38 +5,24 @@ let clickEvent = () => {
     console.log("he")
 }
 
-// inputAll.forEach((item) => {
-//     item.addEventListener('click', clickEvent())
-// });
-
 inputAll.forEach((item) => {
         item.addEventListener('click', function clickEvent(event){
             event.target.classList.add('submitted'); 
         })
     });
     
+//__________________
 
+  document.getElementById("Pass1").addEventListener("change", validatePassword);
+  document.getElementById("Pass2").addEventListener("change", validatePassword);
 
-// document.addEventListener('click', function handleClick(event) {
-//     console.log('user clicked: ', event.target);
-  
-//     event.target.classList.add('bg-yellow');
-//   });
-// function classToForm(){
-//     document.querySelector("input").classList.add("submitted")
-//     console.log("he")
+  function validatePassword(){
+		let pass1 = document.getElementById("Pass1").value;
+    let pass2 = document.getElementById("Pass2").value;
 
-// }
-
-// inputAll.addEventListener("click", classToForm);
-
-
-
-// let elements = document.querySelectorAll('.button');
-
-// let clickEvent = () => {
-//     console.log('some event content here...')
-// }
-// elements.forEach((item) => {
-//     item.addEventListener('click', clickEvent)
-// });
+    if((pass1 != pass2)&&(pass2 != "")) {
+      document.getElementById("errorMsg").innerHTML="Passwords Don\'t Match";
+    	} else {
+        document.getElementById('errorMsg').innerHTML='';
+    }   
+  }
